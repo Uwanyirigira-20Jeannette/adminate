d8bfaa34eddd9a554eeb0d66c72aebe34fd5d609
Uwanyirigira-20jeannette <jannetuwanyirigira2003@gmail.com>
2023-11-05 12:47:32 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,13 @@
+num1=10
+num2=20
+result=num1+num2
+print("the sum of",{num1},"and",{num2},"is",result)
+
+
+orginal_string="I like python"
+reversed_string=orginal_string[::-1]
+print(reversed_string)
+
+num_str=45
+num_int=int(num_str)
+print(type,num_int)
